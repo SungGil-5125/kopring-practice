c2bcff4b376fd91212c9a69e5dfffd8916c8c1ea
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-11 12:21:00 +0900

    :sparkles: :: 토큰 발급, 필터 추가

--- src/main/kotlin/com/project/kopring/global/security/filter/JwtRequestFilter.kt
+++ src/main/kotlin/com/project/kopring/global/security/filter/JwtRequestFilter.kt
@@ -1,4 +1,42 @@
 package com.project.kopring.global.security.filter
 
-class JwtRequestFilter {
+import com.project.kopring.global.security.authentication.AuthDetailService
+import com.project.kopring.global.security.jwt.JwtTokenProvider
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class JwtRequestFilter(
+        private val jwtTokenProvider: JwtTokenProvider,
+        private val authDetailService: AuthDetailService
+): OncePerRequestFilter() {
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        val accessToken: String? = request.getHeader("Authorization")
+
+        if(accessToken != null) {
+            val email: String = jwtTokenProvider.getUserEmail(accessToken);
+            registerUserInfoSecurityContext(email, request)
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    fun registerUserInfoSecurityContext(email: String, request: HttpServletRequest) {
+        try {
+            val userDetails: UserDetails = authDetailService.loadUserByUsername(email)
+            val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(userDetails, null, null)
+            usernamePasswordAuthenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+            SecurityContextHolder.getContext().authentication = (usernamePasswordAuthenticationToken)
+        } catch (e: NullPointerException) {
+            throw RuntimeException()
+        }
+    }
 }
--- src/main/kotlin/com/project/kopring/global/security/jwt/JwtTokenProvider.kt
+++ src/main/kotlin/com/project/kopring/global/security/jwt/JwtTokenProvider.kt
@@ -1,4 +1,67 @@
 package com.project.kopring.global.security.jwt
 
-class JwtTokenProvider{
+import com.project.kopring.global.security.jwt.properties.JwtProperties
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.security.Key
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+@Component
+class JwtTokenProvider(
+        private val jwtProperties: JwtProperties
+){
+    private val ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 3L // 3시간
+    private val REFRESH_TOKEN_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000L // 1주
+
+    enum class TokenType(val value: String) {
+        ACCESS_TOKEN("accessToken"),
+        REFRESH_TOKEN("refreshToken")
+    }
+
+    private fun getSignInKey(secretKey: String): Key {
+        val bytes = secretKey.toByteArray(StandardCharsets.UTF_8)
+        return Keys.hmacShaKeyFor(bytes)
+    }
+
+    fun extractAllClaims(token: String) : Claims {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey(jwtProperties.key))
+                .build()
+                .parseClaimsJws(token)
+                .body
+    }
+
+    fun getUserEmail(token: String): String = extractAllClaims(token).subject
+
+    fun isExpired(token: String): Boolean {
+        return try {
+            extractAllClaims(token).expiration
+            false
+        } catch (e: ExpiredJwtException) {
+            true
+        }
+    }
+
+    fun getExpireAt(): LocalDateTime = LocalDateTime.now().plusSeconds(ACCESS_TOKEN_EXPIRE_TIME / 1000)
+
+    fun doGenerateToken(email: String, tokenType: TokenType, expiredTime: Long): String {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("tokenType", tokenType.value)
+                .setIssuedAt(Date(System.currentTimeMillis()))
+                .setExpiration(Date(System.currentTimeMillis() + expiredTime))
+                .signWith(getSignInKey(jwtProperties.key))
+                .compact()
+    }
+
+    fun generateAccessToken(email: String) : String = doGenerateToken(email, TokenType.ACCESS_TOKEN, ACCESS_TOKEN_EXPIRE_TIME)
+
+    fun generateRefreshToken(email: String) : String = doGenerateToken(email, TokenType.REFRESH_TOKEN, REFRESH_TOKEN_EXPIRE_TIME)
+
 }
