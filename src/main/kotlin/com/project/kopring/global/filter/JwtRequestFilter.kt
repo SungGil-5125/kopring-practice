7be709eb0a627c8cef44e2a481e576bd4e491e50
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-14 16:47:33 +0900

    :sparkles: :: 중복 파일 제거

---
+++ src/main/kotlin/com/project/kopring/global/filter/JwtRequestFilter.kt
@@ -0,0 +1,41 @@
+package com.project.kopring.global.filter
+
+import com.project.kopring.global.security.authentication.AuthDetailService
+import com.project.kopring.global.security.jwt.JwtTokenProvider
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+@Component
+class JwtRequestFilter(
+        private val jwtTokenProvider: JwtTokenProvider,
+        private val authDetailService: AuthDetailService
+): OncePerRequestFilter() {
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        val accessToken = request.getHeader("Authorization")
+
+        if(accessToken != null) {
+            val email = jwtTokenProvider.getUserEmail(accessToken)
+            registerUserInfoSecurityContext(email)
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    fun registerUserInfoSecurityContext(email: String) {
+        try {
+            val userDetails = authDetailService.loadUserByUsername(email)
+            val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(userDetails, null, null)
+            SecurityContextHolder.getContext().authentication = usernamePasswordAuthenticationToken
+        } catch (e: NullPointerException) {
+            throw RuntimeException()
+        }
+    }
+
+}
