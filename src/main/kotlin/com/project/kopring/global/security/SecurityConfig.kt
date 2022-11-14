3605cecda9b07738d239b841cb1966940ea27be0
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-11-14 16:48:22 +0900

    :sparkles: :: Security 설정

--- src/main/kotlin/com/project/kopring/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/project/kopring/global/security/SecurityConfig.kt
@@ -1,12 +1,45 @@
 package com.project.kopring.global.security
 
+import com.project.kopring.global.filter.ExceptionFilter
+import com.project.kopring.global.filter.JwtRequestFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig(
+        private val jwtRequestFilter: JwtRequestFilter,
+        private val exceptionFilter: ExceptionFilter
+) {
+
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable()
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                .anyRequest().authenticated()
+        http
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
+                .addFilterBefore(exceptionFilter, JwtRequestFilter::class.java)
+
+        return http.build()
+    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
