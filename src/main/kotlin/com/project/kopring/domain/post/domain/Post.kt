8137e81e54a638175561351f797ff41b8761e1ee
SungGil-5125 <sunggil0125@naver.com>
2022-12-02 12:25:25 +0900

    :sparkles: :: 생성자로 id 이동

--- src/main/kotlin/com/project/kopring/domain/post/domain/Post.kt
+++ src/main/kotlin/com/project/kopring/domain/post/domain/Post.kt
@@ -6,21 +6,18 @@ import com.project.kopring.global.entity.BaseTimeEntity
 import javax.persistence.*
 
 @Entity
-open class Post(
-        var title: String,
-        var content: String,
-        @ElementCollection(fetch = FetchType.LAZY)
-        @CollectionTable(name = "post_tags", joinColumns = [JoinColumn(name = "post_id")])
-        var tags: MutableList<String>,
-        @ManyToOne
-        @JoinColumn(name = "user_id")
-        val user: User
+class Post(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val postId: Long,
+    var title: String,
+    var content: String,
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "post_tags", joinColumns = [JoinColumn(name = "post_id")])
+    var tags: MutableList<String>,
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: User
 ): BaseTimeEntity() {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")
-    val postId = 0L
 
     fun updatePost(postDto: PostDto) {
         this.title = postDto.title
