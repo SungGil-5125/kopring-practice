6c1df58f3f8fee72b56fc0a8057c7bf38dd80260
SungGil-5125 <sunggil0125@naver.com>
2022-12-05 20:47:15 +0900

    :sparkles: :: oneDelete 옵션 추가

--- src/main/kotlin/com/project/kopring/domain/comment/domain/Comment.kt
+++ src/main/kotlin/com/project/kopring/domain/comment/domain/Comment.kt
@@ -2,6 +2,8 @@ package com.project.kopring.domain.comment.domain
 
 import com.project.kopring.domain.post.domain.Post
 import com.project.kopring.domain.user.domain.User
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import javax.persistence.*
 
 @Entity
@@ -9,11 +11,16 @@ class Comment(
     @Id @GeneratedValue
     @Column(name = "comment_id")
     val id: Long,
-    val comment: String,
+    var comment: String,
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "post_id")
     val post: Post,
     @ManyToOne
     @JoinColumn(name = "user_id")
     val user: User
-)
+) {
+    fun updateComment(comment: String) {
+        this.comment = comment
+    }
+}
