50ce7dcd0914596ff36768596ec5ec890683d7cd
SungGil-5125 <sunggil0125@naver.com>
2022-12-12 10:04:23 +0900

    :sparkles: :: 게시글 테스트 추가

--- src/test/kotlin/com/project/kopring/domain/post/controller/PostControllerTest.kt
+++ src/test/kotlin/com/project/kopring/domain/post/controller/PostControllerTest.kt
@@ -1,28 +1,37 @@
-package com.project.kopring.domain.post.service
+package com.project.kopring.domain.post.controller
 
-import com.project.kopring.domain.post.presentation.data.dto.PostDto
-import com.project.kopring.domain.post.presentation.data.dto.PostKeywordDto
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.project.kopring.domain.post.presentation.data.request.PostRequest
 import com.project.kopring.domain.user.domain.repository.UserRepository
 import com.project.kopring.domain.user.presentation.data.dto.UserDto
 import com.project.kopring.domain.user.service.UserAuthService
 import com.project.kopring.global.security.authentication.AuthDetailService
 import com.project.kopring.global.security.jwt.JwtTokenProvider
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.io.File
 import java.io.FileInputStream
 import java.util.*
 
 @SpringBootTest
-class PostServiceTest(
+@AutoConfigureMockMvc
+class PostControllerTest(
     @Autowired
-    private val postService: PostService,
+    private val mockMvc: MockMvc,
     @Autowired
     private val userAuthService: UserAuthService,
+    @Autowired
+    private val objectMapper: ObjectMapper,
     @Autowired
     private val authDetailService: AuthDetailService,
     @Autowired
@@ -31,7 +40,7 @@ class PostServiceTest(
     private val userRepository: UserRepository
 ) {
 
-    private val email = "test@test.com"
+    private val email = "test5/@test.com"
 
     private val password = "test password"
 
@@ -51,8 +60,7 @@ class PostServiceTest(
         fileName, "testImage.jpeg", contentType, FileInputStream(File(filePath))
     )
 
-    @BeforeEach
-    fun initUser() {
+    fun getAccessToken(): String {
         val userDto = UserDto(
             userId = -1,
             email = email,
@@ -68,6 +76,8 @@ class PostServiceTest(
         val userDetails = authDetailService.loadUserByUsername(jwtTokenProvider.getUserEmail(tokenResponse.accessToken))
         val authentication = UsernamePasswordAuthenticationToken(userDetails, "", null)
         SecurityContextHolder.getContext().authentication = authentication
+
+        return tokenResponse.accessToken
     }
 
     @AfterEach
@@ -78,91 +88,29 @@ class PostServiceTest(
     @Test
     fun `게시글 생성 테스트`() {
         // given
-        val postDto = PostDto(
-            id = -1,
+        val postRequest = PostRequest(
             title = title,
             content = content,
             tags = Collections.emptyList(),
-            file = file
         )
 
-        // when & then
-        assertDoesNotThrow {
-            postService.writePost(postDto)
-        }
-    }
+        val request = objectMapper.writeValueAsString(postRequest)
 
-    @Test
-    fun `게시글 수정 테스트`() {
-        // given
-        val postDto = PostDto(
-            id = 3,
-            title = title,
-            content = content,
-            tags = Collections.emptyList(),
-            file = null
-        )
+        val json = MockMultipartFile("request", "jsondata", "application/json", request.toByteArray())
 
         // when & then
-        assertDoesNotThrow {
-            postService.updatePost(postDto)
-        }
-    }
+        mockMvc.perform(multipart("/post")
+            .file(file)
+            .file(json)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
 
-    @Test
-    fun `게시글 삭제 테스트`() {
-        // given
-        val postDto = PostDto(
-            id = 3,
-            title = "",
-            content = "",
-            tags = Collections.emptyList(),
-            file = null
-        )
+            .header("Authorization", getAccessToken())
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .characterEncoding("UTF-8"))
 
-        // when & then
-        assertDoesNotThrow {
-            postService.deletePost(postDto)
-        }
-    }
-
-    @Test
-    fun `게시글 상세보기 테스트`() {
-        // given
-        val postDto = PostDto(
-            id = 4,
-            title = "",
-            content = "",
-            tags = Collections.emptyList(),
-            file = null
-        )
-
-        // when & then
-        Assertions.assertNotNull {
-            postService.findPostById(postDto)
-        }
-    }
-
-    @Test
-    fun `게시글 전체조회 테스트`() {
-
-        Assertions.assertNotNull {
-            postService.findAllPost()
-        }
-
-    }
-
-    @Test
-    fun `게시글 검색 테스트`() {
-        // given
-        val keywordDto = PostKeywordDto(
-            keyword = "test"
-        )
-
-        // when & then
-        Assertions.assertNotNull {
-            postService.findPostByKeyword(keywordDto)
-        }
+            .andExpect(status().isCreated)
     }
 
 }
